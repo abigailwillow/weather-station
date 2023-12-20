4daab70fd8f7d2022399355a42ede385b46340b8
Abigail de Joode <contact@abbydiode.com>
2023-12-20 15:42:01 +0000

    Create Measurement.cs

---
+++ Models/Measurement.cs
@@ -0,0 +1,27 @@
+﻿namespace WeatherStation.Models;
+
+public class Measurement {
+    public int Id;
+    public string Name;
+    public float Latitude;
+    public float Longitude;
+    public string Region;
+    public DateTime Time;
+    public string Description;
+    public string IconURL;
+    public string FullIconURL;
+    public string GraphURL;
+    public string WindDirection;
+    public float Temperature;
+    public float GroundTemperature;
+    public float FeelTemperature;
+    public float WindGusts;
+    public float WindSpeed;
+    public int WindSpeedBeaufort;
+    public int Humidity;
+    public float Percipitation;
+    public float SunPower;
+    public float RainFall24Hour;
+    public float RainFall1Hour;
+    public int WindDirectionDegrees;
+}
