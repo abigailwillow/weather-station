27c763f7a5aa52df2a48789bb0df39305592eb63
Abigail de Joode <contact@abbydiode.com>
2025-04-10 21:33:14 +0200

    Update to System.Text.Json package

--- Measurements.cs
+++ Measurements.cs
@@ -1,13 +1,17 @@
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
 namespace WeatherStation;
 
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
+using System.Text.Json;
 using WeatherStation.Models;
 using WeatherStation.Utilities;
 
 public class Measurements {
+    const string WEATHER_API_URL = "https://data.buienradar.nl/2.0/feed/json";
+    const string IMAGE_API_URL = "http://picsum.photos/1024/1024";
     private readonly ILogger log;
 
     public Measurements(ILoggerFactory loggerFactory) {
@@ -16,41 +20,40 @@ public class Measurements {
 
     [Function("Measurements")]
     public async Task<HttpResponseData> GetMeasurements([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "measurements")] HttpRequestData request) {
-        const string URL = "https://data.buienradar.nl/2.0/feed/json";
-
         HttpResponseData response = request.CreateResponse();
-
-        string json;
-        HttpClient client = new();
-        HttpResponseMessage httpResponse = await client.GetAsync(URL);
-        httpResponse.EnsureSuccessStatusCode();
-        json =  await httpResponse.Content.ReadAsStringAsync();
-
-        JObject weatherData = JObject.Parse(json);
-        List<JToken> measurementsData = weatherData["actual"]["stationmeasurements"].Children().ToList();
-        List<Measurement> measurements = new List<Measurement>();
-        foreach (JToken measurementData in measurementsData) {
-            Measurement measurement = measurementData.ToObject<Measurement>();
-            measurements.Add(measurement);
-        }
-
-        string firstStationName = measurements[0].stationname;
-        string firstTemperature = $"{measurements[0].temperature}°C";
-
-        byte[] image = await GetRandomImage();
-        image = ImageHelper.AddTextToImage(image, (firstStationName, (16, 16), 32, "#FFFFFF"));
-        image = ImageHelper.AddTextToImage(image, (firstTemperature, (16, 48), 32, "#FFFFFF"));
-
-        await response.WriteBytesAsync(image);
-
+        response.WriteString(JsonSerializer.Serialize(new {x = Guid.NewGuid().ToString()}));
         return response;
     }
 
-    private static async Task<byte[]> GetRandomImage() {
-        const string UNSPLASH_URL = "https://source.unsplash.com/random";
-
+    [Function("GenerateImage")]
+    public async Task<string> GenerateImage([QueueTrigger("images")] Guid jobUuid, FunctionContext context) {
         HttpClient client = new();
-        HttpResponseMessage response = await client.GetAsync(UNSPLASH_URL);
+        HttpResponseMessage httpResponse = await client.GetAsync(WEATHER_API_URL);
+        httpResponse.EnsureSuccessStatusCode();
+        string json =  await httpResponse.Content.ReadAsStringAsync();
+
+        JsonDocument weatherData = JsonDocument.Parse(json);
+        JsonElement measurementsData = weatherData.RootElement.GetProperty("actual").GetProperty("stationmeasurements");
+        List<Measurement> measurements = new List<Measurement>();
+        foreach (JsonElement measurementData in measurementsData.EnumerateArray()) {
+            Measurement measurement = JsonSerializer.Deserialize<Measurement>(measurementData);
+            measurements.Add(measurement);
+        }
+
+        string firstStationName = measurements[0].StationName;
+        string firstTemperature = $"{measurements[0].Temperature}Â°C";
+        
+        byte[] image = await GetRandomImage();
+        image = ImageUtility.AddTextToImage(image, (firstStationName, (16, 16), 32, "#FFFFFF"));
+        image = ImageUtility.AddTextToImage(image, (firstTemperature, (16, 48), 32, "#FFFFFF"));
+        
+        log.LogInformation(image.ToString());
+        return string.Empty;
+    }
+
+    private async Task<byte[]> GetRandomImage() {
+        HttpClient client = new();
+        HttpResponseMessage response = await client.GetAsync(IMAGE_API_URL);
         return await response.Content.ReadAsByteArrayAsync();
     }
 }
--- Models/Measurement.cs
+++ Models/Measurement.cs
@@ -1,9 +1,15 @@
-﻿namespace WeatherStation.Models;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WeatherStation.Models;
 
 public class Measurement {
-    public int stationid { get; set; }
-    public string stationname { get; set; }
-    public float temperature { get; set; }
+    [JsonPropertyName("stationid")]
+    public int StationId { get; set; }
 
-    public Measurement(int id, string name, float temperature) => (this.stationid, this.stationname, this.temperature) = (id, name, temperature);
+    [JsonPropertyName("stationname")]
+    public string StationName { get; set; }
+
+    [JsonPropertyName("temperature")]
+    public float Temperature { get; set; }
 }
