d13b87146277aeea0580783d35cc62176719c3a2
Abigail de Joode <contact@abbydiode.com>
2025-04-13 16:14:19 +0200

    Add messages to queue when measurements endpoint is called

--- Measurements.cs
+++ Measurements.cs
@@ -1,4 +1,4 @@
-using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Azure.Storage.Queues;
 
 namespace WeatherStation;
 
@@ -12,21 +12,30 @@ using WeatherStation.Utilities;
 public class Measurements {
     const string WEATHER_API_URL = "https://data.buienradar.nl/2.0/feed/json";
     const string IMAGE_API_URL = "http://picsum.photos/1024/1024";
-    private readonly ILogger log;
+    private readonly ILogger _log;
+    private readonly QueueClient _queue;
 
-    public Measurements(ILoggerFactory loggerFactory) {
-        log = loggerFactory.CreateLogger<Measurements>();
+    public Measurements(ILoggerFactory loggerFactory, QueueServiceClient queueServiceClient) {
+        _log = loggerFactory.CreateLogger<Measurements>();
+        _queue = queueServiceClient.GetQueueClient("image-generation-queue");
+        _queue.CreateIfNotExists();
     }
 
-    [Function("Measurements")]
+    [Function(nameof(GetMeasurements))]
     public async Task<HttpResponseData> GetMeasurements([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "measurements")] HttpRequestData request) {
+        // TODO: Implement base url resolving logic
+        string jobId = Guid.NewGuid().ToString();
+        string jobUrl = $"https://example.com/api/jobs/{jobId}";
+
+        _queue.SendMessageAsync(JsonSerializer.Serialize(new {jobId = jobId}));
+        
         HttpResponseData response = request.CreateResponse();
-        response.WriteString(JsonSerializer.Serialize(new {x = Guid.NewGuid().ToString()}));
+        response.WriteString(JsonSerializer.Serialize(new {job = jobUrl}));
         return response;
     }
 
-    [Function("GenerateImage")]
-    public async Task<string> GenerateImage([QueueTrigger("images")] Guid jobUuid, FunctionContext context) {
+    [Function(nameof(GenerateImage))]
+    public async Task<string> GenerateImage([QueueTrigger("image-generation-queue")] string jobId, FunctionContext context) {
         HttpClient client = new();
         HttpResponseMessage httpResponse = await client.GetAsync(WEATHER_API_URL);
         httpResponse.EnsureSuccessStatusCode();
@@ -47,11 +56,11 @@ public class Measurements {
         image = ImageUtility.AddTextToImage(image, (firstStationName, (16, 16), 32, "#FFFFFF"));
         image = ImageUtility.AddTextToImage(image, (firstTemperature, (16, 48), 32, "#FFFFFF"));
         
-        log.LogInformation(image.ToString());
+        _log.LogInformation(image.ToString());
         return string.Empty;
     }
 
-    private async Task<byte[]> GetRandomImage() {
+    private static async Task<byte[]> GetRandomImage() {
         HttpClient client = new();
         HttpResponseMessage response = await client.GetAsync(IMAGE_API_URL);
         return await response.Content.ReadAsByteArrayAsync();
