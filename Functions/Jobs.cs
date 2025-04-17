7fe28c66708cea4ff80fcc1d9b008453350ca6fa
Abigail de Joode <contact@abbydiode.com>
2025-04-18 01:07:39 +0200

    Create individual functions

---
+++ Functions/Jobs.cs
@@ -0,0 +1,80 @@
+﻿using WeatherStation.Services;
+
+namespace WeatherStation.Functions;
+
+using System.Text.Json;
+
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+using WeatherStation.Models;
+
+public class Jobs(ILoggerFactory loggerFactory,  IQueueProviderService queueProviderService, IBlobProviderService blobProviderService)
+{
+    private readonly ILogger _logger = loggerFactory.CreateLogger<Jobs>();
+    private readonly QueueClient _queue = queueProviderService.GetQueueClient();
+    private readonly BlobContainerClient _blob = blobProviderService.GetBlobContainerClient();
+
+    private bool IsValidImage(string jobId, int measurementIndex)
+    {
+        BlobClient imageBlobClient = _blob.GetBlobClient($"{jobId}-{measurementIndex}.png");
+        return imageBlobClient.Exists();
+    }
+    
+    private int GetValidImageCount(string jobId)
+    {
+        int imageCount = 0;
+
+        while (IsValidImage(jobId, imageCount)) {
+            imageCount++;
+        };
+
+        return imageCount;
+    }
+    
+    private int GetImageCount(string jobId)
+    {
+        BlobClient metaBlobClient = _blob.GetBlobClient($"{jobId}.json");
+        if (!metaBlobClient.Exists()) return 0;
+        
+        string jsonString = metaBlobClient.DownloadContent().Value.Content.ToString();
+        JobMetadata? metadata = JsonSerializer.Deserialize<JobMetadata>(jsonString);
+        return metadata?.StationCount ?? 0;
+    }
+    
+    [Function(nameof(Jobs))]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "jobs/{jobId}")] HttpRequestData request, string jobId)
+    {
+        _logger.LogInformation("User tried to get job {JobID}", jobId);
+        
+        JobsHttpResponse response = new() { Images = [] };
+
+        int imageCount = GetImageCount(jobId);
+        int validImageCount = GetValidImageCount(jobId);
+        
+        if (validImageCount > 0)
+        {
+            response.Status = validImageCount < imageCount ? JobStatus.PENDING : JobStatus.COMPLETED;
+
+            for (int measurementIndex = 0; measurementIndex < validImageCount; measurementIndex++)
+            {
+                BlobClient imageBlobClient = _blob.GetBlobClient($"{jobId}-{measurementIndex}.png");
+                string imageUrl = imageBlobClient.Uri.ToString();
+                response.Images.Add(imageUrl);
+            }
+        }
+        else
+        {
+            response.Status = imageCount > 0 ? JobStatus.PENDING : JobStatus.INVALID;
+        }
+
+        HttpResponseData responseData = request.CreateResponse();
+        await responseData.WriteStringAsync(JsonSerializer.Serialize(response));
+        return responseData;
+    }
+}
---
+++ Functions/RequestImages.cs
@@ -0,0 +1,70 @@
+﻿namespace WeatherStation.Functions;
+
+using System.Text;
+using System.Text.Json;
+
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Queues;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+using WeatherStation.Models;
+using WeatherStation.Extensions;
+using WeatherStation.Services;
+
+public class RequestImages(ILoggerFactory loggerFactory, IQueueProviderService queueProviderService, IBlobProviderService blobProviderService)
+{
+    private const string WEATHER_API_URL = "https://data.buienradar.nl/2.0/feed/json";
+    
+    private readonly ILogger _logger = loggerFactory.CreateLogger<RequestImages>();
+    private readonly QueueClient _queue = queueProviderService.GetQueueClient();
+    private readonly BlobContainerClient _blob = blobProviderService.GetBlobContainerClient();
+
+    [Function(nameof(RequestImages))]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "POST", Route = "request-images")] HttpRequestData request)
+    {
+        string jobId = Guid.NewGuid().ToString();
+        string jobUrl = $"{request.Url.GetLeftPart(UriPartial.Authority)}/api/jobs/{jobId}";
+
+        HttpClient client = new();
+        HttpResponseMessage httpResponse = await client.GetAsync(WEATHER_API_URL);
+        httpResponse.EnsureSuccessStatusCode();
+        string jsonData = await httpResponse.Content.ReadAsStringAsync();
+        JsonDocument weatherData = JsonDocument.Parse(jsonData);
+        JsonElement measurementsData = weatherData.RootElement.GetProperty("actual").GetProperty("stationmeasurements");
+        
+        BlobClient metaBlobClient = _blob.GetBlobClient($"{jobId}.json");
+        string metadata = JsonSerializer.Serialize(new JobMetadata { StationCount = measurementsData.GetArrayLength() });
+        
+        using MemoryStream memoryStream = new (Encoding.UTF8.GetBytes(metadata));
+        await metaBlobClient.UploadAsync(
+            memoryStream,
+            new BlobHttpHeaders {
+                ContentType = "application/json"
+            }
+        );
+        
+        int measurementIndex = 0;
+        foreach (JsonElement measurementData in measurementsData.EnumerateArray()) {
+            Measurement? measurement = measurementData.Deserialize<Measurement>();
+            _queue.SendMessageString(JsonSerializer.Serialize(new ImageGenerationRequest {
+                JobId = jobId,
+                MeasurementIndex = measurementIndex,
+                StationName = measurement?.StationName ?? string.Empty,
+                Temperature = measurement?.Temperature ?? 0.0f
+            }));
+            
+            measurementIndex += 1;
+        }
+
+        HttpResponseData response = request.CreateResponse();
+        await response.WriteStringAsync(JsonSerializer.Serialize(new ImageGenerationHttpResponse {
+            JobId = jobId,
+            JobUrl = jobUrl
+        }));
+        return response;
+    }
+}
